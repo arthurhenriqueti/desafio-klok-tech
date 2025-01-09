43ef0e75d8104ebc125efc1b5c268902e50da4e7
Arthur Henrique <arthurbleckrep2@gmail.com>
2025-01-09 14:14:43 -0300

    Fazendo alguns ajustes

--- src/test/java/core/BasePage.java
+++ src/test/java/core/BasePage.java
@@ -10,10 +10,6 @@ public class BasePage {
         getDriver().findElement(By.id(idCampo)).sendKeys(texto);
     }
 
-    public void escreverXpath(String xpathCampo, String texto) {
-        getDriver().findElement(By.xpath(xpathCampo)).sendKeys(texto);
-    }
-
     public void clicarBotaoId(String idBotao) {
         getDriver().findElement(By.id(idBotao)).click();
     }
@@ -22,23 +18,9 @@ public class BasePage {
         getDriver().findElement(By.xpath(xpathBotao)).click();
     }
 
-    public void clicarBotaoCss(String cssBotao) {
-        getDriver().findElement(By.cssSelector(cssBotao)).click();
-    }
-
-    public void clicarBotaoClassName(String className) {
-        getDriver().findElement(By.className(className)).click();
-    }
-
     public String obterTextoXpath(String xpathTexto) {
         String element;
         element = getDriver().findElement(By.xpath(xpathTexto)).getText();
-        return element;
-    }
-
-    public String obterTextoId(String idTexto) {
-        String element;
-        element = getDriver().findElement(By.id(idTexto)).getText();
         return element;
     }
 }
--- src/test/java/pages/BuscarProdutoPage.java
+++ src/test/java/pages/BuscarProdutoPage.java
@@ -27,10 +27,6 @@ public class BuscarProdutoPage extends BasePage {
         return obterTextoXpath(xpath);
     }
 
-    public String confirmarProdutoPeloId(String id) {
-        return obterTextoXpath(id);
-    }
-
     public void clicarNoMenu() {
         clicarBotaoId("nav-hamburger-menu");
     }
@@ -54,11 +50,6 @@ public class BuscarProdutoPage extends BasePage {
     public void clicarNaCategoriaCelularesSmartphones() {
         clicarBotaoXpath("/html/body/div[1]/div[2]/div/div/div[2]/div/div/div[2]/div[2]/div[2]/div[3]");
         Assert.assertEquals("Mais Vendidos em Celulares e Smartphones", confirmarProduto("/html/body/div[1]/div[2]/div/div/div[1]/div/div/div[1]/h1"));
-    }
-
-    public void aplicarFiltros() {
-        clicarBotaoXpath("/html/body/div[1]/div[1]/div[2]/div[2]/div/div/div[4]/ul/li[1]/span/a/div/label/i");
-        clicarBotaoXpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div[3]/span/div[1]/span/div/div[2]/div[6]/div[22]/ul/span/span[4]/li/span/a/div/label/i");
     }
 
     public void selecionarProduto(String xpath) throws InterruptedException {
